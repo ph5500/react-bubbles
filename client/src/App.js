17ed83de99641cd31f84def61beb0fbec7ab836b
ph5500 <philfives@gmail.com>
2020-04-24 11:55:08 -0600

    added axios call to bububble page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+// import { PrivateRoute } from "./components/PrivateRoute";
 
 import Login from "./components/Login";
 import "./styles.scss";
+// import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          {/* <PrivateRoute exact path="/protected" component={BubblePage} /> */}
+          <Route exact path="/" component={Login} />
+          <Route component={Login} />
+          {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        </Switch>
       </div>
     </Router>
   );
--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const axiosWithAuth = () => {
     // returns an "instance" of axios, with preconfigured configs
-    const token = JSON.parse(localStorage.getItem('token'));
     return axios.create({
         headers: {
-            Authorization: token
+            Authorization: localStorage.getItem("token")
         },
         baseURL: 'http://localhost:5000'
     });
 
 };
+
